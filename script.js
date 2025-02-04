bd01b48fe504e261f7cfdba8287b3032ea39242b
chizara <chinaza1997@gmail.com>
2025-02-04 22:21:50 +0100

    branch

--- script.js
+++ script.js
@@ -31,7 +31,30 @@ searchForm.addEventListener("submit", (e) =>{
 
 
 
-
+    const accesskey = "ne1Z0kHgjyhB4Nfm22PDWkIUW-q3B6lrTsBD2QURmZ0"
+    const searchForm = document.getElementById("search-form");
+    const searchBox = document.getElementById("search-box");
+    const searchResult = document.getElementById("search-result");
+    const searchMoreBtn = document.getElementById("show-more-btn");
+    
+    
+    let keyword = "";
+    let page = 1;
+    
+    async function searchImages(){
+        keyword = searchBox.value;
+        const url = https://api.unsplash.com/photos?page=${page}&query=${keyword}&client_id=${accesskey};
+    
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    }
+    
+    searchForm.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        page = 1;
+        searchImages();
+    })
 
 
 
